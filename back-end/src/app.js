90a952aa7d48d9ca14868dac3daa6b3c2dcfefc9
azahir95 <alizahir95@gmail.com>
2022-09-01 18:16:41 -0700

    updated server.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
@@ -12,7 +12,7 @@ const tablesRouter = require("./tables/tables.router");
 
 const app = express();
 console.log('hi mom');
-app.use(cors('*'));
+//app.use(cors('*'));
 //app.use(cors({origin: 'https://restaurant-reservation-client-sooty.vercel.app/dashboard'}));
 app.use(express.json());
 app.options("*", cors());
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,6 +3,9 @@ const { PORT = 5000 } = process.env;
 const app = require("./app");
 const knex = require("./db/connection");
 
+const cors = require("cors");
+app.use(cors());
+
 knex.migrate
   .latest()
   .then((migrations) => {
